137406b910caeb8422336976e805fc4465bb1732
Le Nguyen <le.nguyenhoang2910@gmail.com>
2026-01-11 14:22:52 -0700

    Update getData.py
    Adding main() method for ProcessPoolExecutor. When child processes reimport script, it won't run entire thing, reduce overhead. Better time in data fetching.

--- getData.py
+++ getData.py
@@ -1,16 +1,10 @@
+import time 
 import concurrent.futures as c 
 import pandas as pd 
 import requests 
-# import numpy as np 
-# import matplotlib.pyplot as plt
-# from sklearn.linear_model import LinearRegression
-# from sklearn.preprocessing import OneHotEncoder
-# from sklearn.compose import ColumnTransformer
-# from sklearn.model_selection import train_test_split
-#import statsmodels.api as sm
 
 # Take data from file
-data = pd.read_csv("tmdb_5000_credits.csv")
+data = pd.read_csv("./Data/tmdb_5000_credits.csv")
 
 movieID = data["movie_id"] # access the movie ids 
 
@@ -38,12 +32,12 @@ def fetch_genres(movie_id):
     try:
         response = requests.get(url, headers=movie_headers)
         response.raise_for_status()
-        # data = response.json()
-        rating = response.json()['vote_average']
-        genresList = [genre["name"] for genre in response.json().get("genres", [])]
+        rating = response.json()['vote_average'] # get movie rating
+        genresList = [genre["name"] for genre in response.json().get("genres", [])] # parses JSON -> dict object 
+        # then get all genres , else empty list []
 
         for genre in all_genres:
-            movie_data[genre] = int(genre in genresList)
+            movie_data[genre] = int(genre in genresList) # convert boolean to int 1 or 0 for True or False 
         movie_data["vote_average"] = rating
         return str(movie_id), movie_data
     
@@ -76,25 +70,34 @@ def fetch_revenue(movie_id):
     
 # NOTE: Run with multiple processes to fetch data 
 movies_ratings = {}
-movies_revenues = {}
-with c.ProcessPoolExecutor() as executor:
-    results = executor.map(fetch_genres, movieID)
-    results2 = executor.map(fetch_revenue, movieID)
-    for movie_id, movie_data in results:
-        if movie_data:
-            movies_ratings[movie_id] = movie_data
-    for movie_id, movie_data in results2: 
-        if movie_data: 
-            movies_revenues[movie_id] = movie_data
+# movies_revenues = {}
+# Use ProcessPoolExecutor for faster data fetching
+def main(): 
 
-test = pd.DataFrame.from_dict(movies_ratings,orient="index")
-test2 = pd.DataFrame.from_dict(movies_revenues, orient="index")
+    start = time.perf_counter()
+    with c.ProcessPoolExecutor() as executor:
+        results = executor.map(fetch_genres, movieID,chunksize=200)
+        # results2 = executor.map(fetch_revenue, movieID)
+        for movie_id, movie_data in results:
+            if movie_data:
+                movies_ratings[movie_id] = movie_data
+        # for movie_id, movie_data in results2: 
+        #     if movie_data: 
+        #         movies_revenues[movie_id] = movie_data
+    end = time.perf_counter()
+    print(f"Data fetching completed in {end - start:.4f} seconds")
 
+    # from_dict() method: more info when hover over 
+    test = pd.DataFrame.from_dict(movies_ratings,orient="index") # orient="index" means the keys of the dictionary will be the row labels
+                                                                 # so movie_id will be row labels and columns will be genres + vote_average
+    # test2 = pd.DataFrame.from_dict(movies_revenues, orient="index")
+    print(test.head(10))
 
-# NOTE: write Data to excel file 
-# test = pd.DataFrame(movies_ratings) 
-# test.to_excel("movies_genre_data2.xlsx", index_label="movie_id" , sheet_name="Movies Revenue")
-with pd.ExcelWriter('movie_data.xlsx', engine='openpyxl') as writer:
-    test.to_excel(writer, sheet_name='Movie Genres', index_label="movie_id")
-    test2.to_excel(writer, sheet_name='Movie Revenues', index_label="movie_id")
-
+    # NOTE: write Data to excel file 
+    # test = pd.DataFrame(movies_ratings) 
+    # test.to_excel("movies_genre_data2.xlsx", index_label="movie_id" , sheet_name="Movies Revenue")
+    # with pd.ExcelWriter('movie_data.xlsx', engine='openpyxl') as writer:
+    #     test.to_excel(writer, sheet_name='Movie Genres', index_label="movie_id")
+        # test2.to_excel(writer, sheet_name='Movie Revenues', index_label="movie_id")
+if __name__ == "__main__": 
+    main() 
